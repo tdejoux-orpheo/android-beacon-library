872206c6fd8abd973d2a550d156d6ed2671d15e5
Thomas Dejoux <thomasdejoux@iMac-de-Michael.local>
2022-06-07 11:50:59 +0200

    Fix wrong onLeScan override

--- lib/src/main/java/org/altbeacon/beacon/service/scanner/CycledLeScannerForJellyBeanMr2.java
+++ lib/src/main/java/org/altbeacon/beacon/service/scanner/CycledLeScannerForJellyBeanMr2.java
@@ -6,11 +6,15 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.os.SystemClock;
 import android.os.ParcelUuid;
+import android.util.ArrayMap;
+import android.util.Log;
 import androidx.annotation.MainThread;
 import androidx.annotation.WorkerThread;
 
 import org.altbeacon.beacon.logging.LogManager;
 import org.altbeacon.bluetooth.BluetoothCrashResolver;
+import org.altbeacon.bluetooth.BluetoothUuid;
+import org.altbeacon.bluetooth.Pdu;
 
 import java.util.Map;
 
@@ -111,9 +115,9 @@ public class CycledLeScannerForJellyBeanMr2 extends CycledLeScanner {
                     new BluetoothAdapter.LeScanCallback() {
 
                         @Override
-                        public void onLeScan(final BluetoothDevice device, final int rssi,
-                                             final byte[] scanRecord, Map<ParcelUuid, byte[]> serviceData ) {
+                        public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord ) {
                             LogManager.d(TAG, "got record");
+                            Map<ParcelUuid, byte[]> serviceData = parseServiceData(scanRecord);
                             mCycledLeScanCallback.onLeScan(device, rssi, scanRecord, serviceData, System.currentTimeMillis());
                             if (mBluetoothCrashResolver != null) {
                                 mBluetoothCrashResolver.notifyScannedDevice(device, getLeScanCallback());
@@ -122,5 +126,53 @@ public class CycledLeScannerForJellyBeanMr2 extends CycledLeScanner {
                     };
         }
         return leScanCallback;
+    }
+
+    private Map<ParcelUuid, byte[]> parseServiceData(byte[] scanRecord) {
+        int currentPos = 0;
+        Map<ParcelUuid, byte[]> serviceData = new ArrayMap<ParcelUuid, byte[]>();
+        try {
+            while (currentPos < scanRecord.length) {
+                // length is unsigned int.
+                int length = scanRecord[currentPos++] & 0xFF;
+                if (length == 0) {
+                    break;
+                }
+                // Note the length includes the length of the field type itself.
+                int dataLength = length - 1;
+                // fieldType is unsigned int.
+                int fieldType = scanRecord[currentPos++] & 0xFF;
+                switch (fieldType) {
+                    case Pdu.SERVICE_UUID_16_BIT_PDU_TYPE:
+                    case Pdu.SERVICE_UUID_32_BIT_PDU_TYPE:
+                    case Pdu.SERVICE_UUID_128_BIT_PDU_TYPE:
+                        int serviceUuidLength = BluetoothUuid.UUID_BYTES_16_BIT;
+                        if (fieldType == Pdu.SERVICE_UUID_32_BIT_PDU_TYPE) {
+                            serviceUuidLength = BluetoothUuid.UUID_BYTES_32_BIT;
+                        } else if (fieldType == Pdu.SERVICE_UUID_128_BIT_PDU_TYPE) {
+                            serviceUuidLength = BluetoothUuid.UUID_BYTES_128_BIT;
+                        }
+
+                        byte[] serviceDataUuidBytes = extractBytes(scanRecord, currentPos,
+                                serviceUuidLength);
+                        ParcelUuid serviceDataUuid = BluetoothUuid.parseUuidFrom(
+                                serviceDataUuidBytes);
+                        byte[] serviceDataArray = extractBytes(scanRecord,
+                                currentPos + serviceUuidLength, dataLength - serviceUuidLength);
+                        serviceData.put(serviceDataUuid, serviceDataArray);
+                        break;
+                    default:
+                        // Just ignore, we don't handle such data type.
+                        break;
+                }
+                currentPos += dataLength;
+            }
+            return serviceData;
+        } catch (Exception e) {
+            Log.e(TAG, "unable to parse scan data: " + Arrays.toString(scanRecord));
+            // As the record is invalid, ignore all the parsed results for this packet
+            // and return an empty record with raw scanRecord bytes in results
+            return new ArrayMap<ParcelUuid, byte[]>();
+        }
     }
 }
